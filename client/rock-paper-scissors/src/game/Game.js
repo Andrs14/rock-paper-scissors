6991eb1b018de0b373085a41150aa1d178c9d95e
Andres Ortega <adresortegao@gmail.com>
2019-04-27 15:48:02 -0400

    Add basic template, without style, for Game and Ranking

--- client/rock-paper-scissors/src/game/Game.js
+++ client/rock-paper-scissors/src/game/Game.js
@@ -3,7 +3,29 @@ import React from 'react';
 class Game extends React.Component {
     render() {
         return (
-            <p>Here will go the game</p>
+            <div>
+                <div>
+                    <h1>Round [number]</h1>
+                    <h2>[Player name]</h2>
+                    <p>Choose your weapon!</p>
+                    <div>
+                        <button>Rock</button>
+                        <button>Paper</button>
+                        <button>Scissor</button>
+                    </div>
+                    <button>Ok</button>
+                </div>
+                <div>
+                    <h1>Score</h1>
+                    <div>
+                        <div>Round</div><div>Winner</div>
+                    </div>
+                    <div>
+                        <div>#1</div><div>[Player 1 name]</div>
+                    </div>
+                </div>
+            </div>
+
         );
     }
 }
--- client/rock-paper-scissors/src/ranking/Ranking.js
+++ client/rock-paper-scissors/src/ranking/Ranking.js
@@ -3,7 +3,23 @@ import React from 'react';
 class Ranking extends React.Component {
     render() {
         return (
-            <p>Here will go the rankings</p>
+            <div>
+                <h1>Rankings</h1>
+                <table>
+                    <tr>
+                        <th>Place</th>
+                        <th>Player</th>
+                        <th>Number of wins</th>
+                        <th>Favorite weapon</th>
+                    </tr>
+                    <tr>
+                        <td>1</td>
+                        <td>Player 1</td>
+                        <td>28329</td>
+                        <td>Rock</td>
+                    </tr>
+                </table>
+            </div>
         );
     }
 }
