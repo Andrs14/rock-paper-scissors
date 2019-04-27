be1ae057c4991b30c5b4bd276aa1e340b8153ab2
Andres Ortega <adresortegao@gmail.com>
2019-04-27 15:28:25 -0400

    Add basic template, without style, for PlayerForm

--- client/rock-paper-scissors/src/player/PlayerForm.js
+++ client/rock-paper-scissors/src/player/PlayerForm.js
@@ -7,7 +7,20 @@ class PlayerForm extends React.Component {
 
     render() {
         return (
-            <p>Here will go the player form</p>
+            <div>
+                <h1>Who is going to play?</h1>
+                <div>
+                    <label>Player 1</label>
+                    <input/>
+                </div>
+                <div>
+                    <label>Player 2</label>
+                    <input/>
+                </div>
+                <div>
+                    <button>Start!</button>
+                </div>
+            </div>
         );
     }
 }
