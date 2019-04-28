c06e3b5f5518733de75b3a8976e642d908a17a04
Andres Ortega <adresortegao@gmail.com>
2019-04-28 19:38:14 -0400

    Add typeORM and configure it

--- api/rock-paper-scissors/src/app.module.ts
+++ api/rock-paper-scissors/src/app.module.ts
@@ -1,10 +1,25 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { GamesController } from './games/games.controller';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import {config} from 'dotenv';
+config();
 
 @Module({
-  imports: [],
-  controllers: [AppController],
+  imports: [
+    TypeOrmModule.forRoot({
+      type: 'postgres',
+      host: process.env.DB_HOST,
+      port: parseInt(process.env.DB_PORT, 10),
+      username: process.env.DB_USER,
+      password: process.env.DB_PASSWORD,
+      database: process.env.DB_DATABASE,
+      entities: [__dirname + '/**/*.entity{.ts,.js}'],
+      synchronize: true,
+    }),
+  ],
+  controllers: [AppController, GamesController],
   providers: [AppService],
 })
 export class AppModule {}
