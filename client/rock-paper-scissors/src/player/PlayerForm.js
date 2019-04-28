372212be5d601bbace3f3cf82e399b3635554d5c
Andres Ortega <adresortegao@gmail.com>
2019-04-28 14:08:19 -0400

    Test and add behavior to Player form

--- client/rock-paper-scissors/src/player/PlayerForm.js
+++ client/rock-paper-scissors/src/player/PlayerForm.js
@@ -3,24 +3,45 @@ import React from 'react';
 class PlayerForm extends React.Component {
     constructor(props) {
         super(props);
+        this.state = {
+            player1: '',
+            player2: ''
+        };
+
+        this.handleSubmit = this.handleSubmit.bind(this);
+        this.changePlayer1 = this.changePlayer1.bind(this);
+        this.changePlayer2 = this.changePlayer2.bind(this);
+    }
+
+    handleSubmit(event) {
+        this.props.onSubmit(this.state);
+        event.preventDefault();
+    }
+
+    changePlayer1(event) {
+        this.setState({player1: event.target.value});
+    }
+
+    changePlayer2(event) {
+        this.setState({player2: event.target.value});
     }
 
     render() {
         return (
-            <div>
+            <form data-testid="form" onSubmit={this.handleSubmit}>
                 <h1>Who is going to play?</h1>
                 <div>
                     <label>Player 1</label>
-                    <input/>
+                    <input data-testid="player1" type="text" value={this.state.player1} onChange={this.changePlayer1}/>
                 </div>
                 <div>
                     <label>Player 2</label>
-                    <input/>
+                    <input data-testid="player2" type="text" value={this.state.player2} onChange={this.changePlayer2}/>
                 </div>
                 <div>
-                    <button>Start!</button>
+                    <input type="submit" value="Start!"/>
                 </div>
-            </div>
+            </form>
         );
     }
 }
---
+++ client/rock-paper-scissors/src/player/PlayerForm.test.js
@@ -0,0 +1,49 @@
+import React from 'react';
+import PlayerForm from './PlayerForm';
+import {render, fireEvent, cleanup, waitForElement} from 'react-testing-library';
+
+describe('PlayerForm', () => {
+    afterEach(cleanup);
+
+    test('render without crashing', () => {
+        expect(renderComponent()).not.toBeUndefined();
+    });
+
+    function renderComponent(onSubmit) {
+        return render(<PlayerForm onSubmit={onSubmit}></PlayerForm>);
+    }
+
+    function dummyEvent(value) {
+        return {
+            target: {value: value}
+        };
+    }
+
+    test('should handle players submit', () => {
+        const onSubmit = jest.fn();
+        const {getByTestId} = renderComponent(onSubmit);
+
+        const expectedPlayer1 = {
+            testId: 'player1',
+            value: 'oriana'
+        };
+        const expectedPlayer2 = {
+            testId: 'player2',
+            value: 'adres'
+        };
+
+        [expectedPlayer1, expectedPlayer2].forEach(
+            ({testId, value}) => fireEvent.change(getByTestId(testId), dummyEvent(value))
+        );
+
+        fireEvent.submit(getByTestId('form'));
+        
+        expect(onSubmit).toBeCalledWith(
+            expect.objectContaining({
+                player1: expectedPlayer1.value,
+                player2: expectedPlayer2.value
+            })
+        );
+
+    });
+});
