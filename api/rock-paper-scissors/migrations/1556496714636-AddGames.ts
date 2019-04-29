ed6f0e908f608b6ad7fe65c6da10baec12406577
Andres Ortega <adresortegao@gmail.com>
2019-04-29 05:54:22 -0400

    Add round table to migration and fix game migration

--- api/rock-paper-scissors/migrations/1556496714636-AddGames.ts
+++ api/rock-paper-scissors/migrations/1556496714636-AddGames.ts
@@ -5,22 +5,48 @@ export class AddGames1556496714636 implements MigrationInterface {
     public async up(queryRunner: QueryRunner): Promise<any> {
       await queryRunner.query('');
 
-      await queryRunner.createTable(new Table({
-        name: 'games',
+      function createTable(options) {
+        return queryRunner.createTable(new Table(options), true);
+      }
+
+
+      const idColumn = {
+        name: 'id',
+        type: 'uuid',
+        isPrimary: true,
+        isGenerated: true,
+        generationStrategy: 'uuid',
+      };
+      await createTable({
+        name: 'game',
         columns: [
+          idColumn,
           {
-            name: 'id',
-            type: 'uuid',
-            isPrimary: true,
-            isGenerated: true,
-            generationStrategy: 'uuid',
+            name: 'roundsCount',
+            type: 'integer',
           },
         ],
-      }), true);
+      });
+
+      await createTable({
+        name: 'round',
+        columns: [
+          idColumn,
+          {
+            name: 'player1Move',
+            type: 'varchar',
+          },
+          {
+            name: 'player2Move',
+            type: 'varchar',
+          },
+        ],
+      });
     }
 
     public async down(queryRunner: QueryRunner): Promise<any> {
-      await queryRunner.dropTable('games');
+      await queryRunner.dropTable('game');
+      await queryRunner.dropTable('round');
     }
 
 }
