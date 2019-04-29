65d17562fe357a0fda8ff685a5134f43d9800f13
Andres Ortega <adresortegao@gmail.com>
2019-04-29 06:28:32 -0400

    Add missing tables to migration
    Tables:
    - game
    - player
    - round

--- api/rock-paper-scissors/migrations/1556496714636-AddGames.ts
+++ api/rock-paper-scissors/migrations/1556496714636-AddGames.ts
@@ -1,52 +1,105 @@
-import { MigrationInterface, QueryRunner, Table } from 'typeorm';
+import { MigrationInterface, QueryRunner, Table, TableForeignKey } from 'typeorm';
 
 export class AddGames1556496714636 implements MigrationInterface {
 
-    public async up(queryRunner: QueryRunner): Promise<any> {
-      await queryRunner.query('');
+  private table: string;
 
-      function createTable(options) {
-        return queryRunner.createTable(new Table(options), true);
-      }
+  public async up(queryRunner: QueryRunner): Promise<any> {
+    await queryRunner.query('');
 
+    const idColumn = {
+      name: 'id',
+      type: 'uuid',
+      isPrimary: true,
+      isGenerated: true,
+      generationStrategy: 'uuid',
+    };
 
-      const idColumn = {
-        name: 'id',
-        type: 'uuid',
-        isPrimary: true,
-        isGenerated: true,
-        generationStrategy: 'uuid',
-      };
-      await createTable({
-        name: 'game',
-        columns: [
-          idColumn,
-          {
-            name: 'roundsCount',
-            type: 'integer',
-          },
-        ],
-      });
+    await createTable({
+      name: 'player',
+      columns: [
+        idColumn,
+        {
+          name: 'name',
+          type: 'varchar',
+        },
+      ],
+    });
 
-      await createTable({
-        name: 'round',
-        columns: [
-          idColumn,
-          {
-            name: 'player1Move',
-            type: 'varchar',
-          },
-          {
-            name: 'player2Move',
-            type: 'varchar',
-          },
-        ],
-      });
+    await createTable({
+      name: 'game',
+      columns: [
+        idColumn,
+        {
+          name: 'roundsCount',
+          type: 'integer',
+        },
+        {
+          name: 'player1Id',
+          type: 'uuid',
+        },
+        {
+          name: 'player2Id',
+          type: 'uuid',
+        },
+      ],
+    });
+
+    await createForeignKey('game',{
+      columnNames: ['player1Id'],
+      referencedColumnNames: ['id'],
+      referencedTableName: 'player',
+    });
+
+    await createForeignKey('game',{
+      columnNames: ['player2Id'],
+      referencedColumnNames: ['id'],
+      referencedTableName: 'player',
+    });
+
+    const gameIdColumn = {
+      name: 'gameId',
+      type: 'uuid',
+    };
+
+    await createTable({
+      name: 'round',
+      columns: [
+        idColumn,
+        gameIdColumn,
+        {
+          name: 'player1Move',
+          type: 'varchar',
+        },
+        {
+          name: 'player2Move',
+          type: 'varchar',
+        },
+      ],
+    });
+
+    const gameIdForeignKey = {
+      columnNames: ['gameId'],
+      referencedColumnNames: ['id'],
+      referencedTableName: 'game',
+    };
+
+    await createForeignKey('round', gameIdForeignKey);
+
+    function createTable(options) {
+      return queryRunner.createTable(new Table(options), true);
     }
 
-    public async down(queryRunner: QueryRunner): Promise<any> {
-      await queryRunner.dropTable('game');
-      await queryRunner.dropTable('round');
+    function createForeignKey(table, options) {
+      return queryRunner.createForeignKey(table, new TableForeignKey(options));
     }
+  }
+
+  public async down(queryRunner: QueryRunner): Promise<any> {
+    await queryRunner.dropTable('result');
+    await queryRunner.dropTable('round');
+    await queryRunner.dropTable('game');
+    await queryRunner.dropTable('player');
+  }
 
 }
