da897c1f29d5b759a4d256862e6b5bb0ebf70268
Andres Ortega <adresortegao@gmail.com>
2019-04-29 00:52:16 -0400

    Add games entity to module

--- api/rock-paper-scissors/src/games/games.module.ts
+++ api/rock-paper-scissors/src/games/games.module.ts
@@ -1,6 +1,9 @@
 import { Module } from '@nestjs/common';
+import { Game } from './Game';
+import { TypeOrmModule } from '@nestjs/typeorm';
 
 @Module({
+  imports: [TypeOrmModule.forFeature(Game)],
   providers: [],
 })
 export class GamesModule {}
